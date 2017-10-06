d3a868bc7a3e2c61416d0621013ce876ae3fa6a9
Peter Tatkowski <tatkows2@illinois.edu>
2017-10-06 14:51:27 +0200

    more skeele

--- simulator.py
+++ simulator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import queue 
 
 class Intersecton:
   def __init__(self,lanes):
@@ -6,21 +7,26 @@ class Intersecton:
     #This is for where the cars actually cross each other
     #Assume cars are two units long, one unit wide
     self.grid = np.zeros((lanes*2,lanes*2))
-    self.map = np.zeros((200,200)) #for coming up to the intersection
+    self.lanes = [[],[],[],[]] # coming FROM N,E,S,W  (Direction,lane,car)
+    for i in range(4):
+      for j in range(lanes):
+        self.lanes[i].append([])
 
 class Car:
-  def __init__(self,number,posX,posY,direction):
-    self.number = number #identifier
-    self.speed = 3 #move 3 tiles at a time. Speed goes from 0-3
-    self.posX = posX #position of FRONT bumper
-    self.posY = posY #position of FRONT bumper
-    self.direction = direction #direction of travel
+  totalCars = 0
+  
+  def __init__(self):
+    self.number = totalCars #identifier
+    self.speed = 3 # move 3 tiles at a time. Speed goes from 0-3
+    self.pos = 100 # 100 tiles to the intersection
+    totalCars += 1
   
   def speedUp(self):
-    pass
+    if self.speed < 3:
+      self.speed += 1
     
   def slowDown(self):
-    pass
+    
     
   def reserve(self,Intersection):
     #See paper. This reserves a space in the intersection
@@ -30,7 +36,7 @@ def spawnCar():
   pass
 
 def runSimulator(i):
-  pass
+  
 
 def main():
   Intersection i(1) #1 lane
